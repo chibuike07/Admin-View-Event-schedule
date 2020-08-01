46c3fd48e47f5f570b1340ce97a0272bbff14fb5
hobbyluv <princewillchime43@gmail.com>
2020-08-01 15:14:54 +0100

    set the section part of the header view

--- src/Pages/pages_landing_page/landing_page.jsx
+++ src/Pages/pages_landing_page/landing_page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faUsers, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import CustomButton from "../../Components/component_button/Button";
 import CustomNavLink from "../../Components/component_navlinks/NavLink";
 import Styles from "./landing_page.module.css";
 const Landing_page = () => {
@@ -12,6 +13,15 @@ const Landing_page = () => {
     icon,
     navbar,
     text,
+    heading,
+    section,
+    section_wrapper,
+    section_icon,
+    icon_text,
+    hint,
+    button,
+    highlight,
+    btn,
   } = Styles;
   return (
     <div className={container}>
@@ -49,6 +59,34 @@ const Landing_page = () => {
             />
           </nav>
         </div>
+
+        <section className={section}>
+          <div className={section_wrapper}>
+            <div className={heading}>
+              <h4>think chat</h4>
+            </div>
+            <div className={hint}>
+              <p>online chat which provides it's...</p>
+            </div>
+            <div className={highlight}>
+              <div className={section_icon}>
+                <FontAwesomeIcon icon={faUserAlt} color="#fff" />
+              </div>
+              <p className={icon_text}>
+                we do make our learning intensive for the users. data sent here
+                are intensively on the basis of this group.
+              </p>
+            </div>
+            <div className={button}>
+              <CustomButton
+                text={"ongoing event"}
+                padding={"10%"}
+                className={btn}
+                backgroundColor={"rgb(0, 102, 102)"}
+              />
+            </div>
+          </div>
+        </section>
       </header>
     </div>
   );
