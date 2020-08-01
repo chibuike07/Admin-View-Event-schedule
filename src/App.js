95295233ac15cd32dfd54077a96c982e5a2ad6a7
hobbyluv <princewillchime43@gmail.com>
2020-08-01 10:56:10 +0100

    adding font awesome api for icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Event from "./Pages/Pages_event/event.jsx";
 import LoadEvent from "./Pages/Pages_load_post/load_event_params.jsx";
+import LandingPage from "./Pages/pages_landing_page/landing_page.jsx";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route path="/" component={Event} exact />
           <Route path="/load_event/:id" component={LoadEvent} exact />
+          <Route path="/landing_page/" component={LandingPage} exact />
         </Switch>
       </BrowserRouter>
     </div>
--- src/Pages/pages_landing_page/landing_page.jsx
+++ src/Pages/pages_landing_page/landing_page.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import CustomImage from "../../Components/component_image/Image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import CustomNavLink from "../../Components/component_navlinks/NavLink";
-const anding_page = () => {
+import Styles from "./landing_page.module.css";
+const Landing_page = () => {
+  const { container, header, left_wrapper, icon, navbar, text } = Styles;
   return (
-    <div>
-      <header className="header">
-        <div className="left_wrapper">
-          <div className="image">
-            <CustomImage src={} alt={"Logo"} />
+    <div className={container}>
+      <header className={header}>
+        <div className={left_wrapper}>
+          <div className={icon}>
+            <FontAwesomeIcon icon={faUsers} />
           </div>
-          <div className="text">
+          <div className={text}>
             <p>group chat</p>
           </div>
         </div>
-        <nav className="navbar">
-          <CustomNavLink text={"home"} />
-          <CustomNavLink text={"contact"} />
-          <CustomNavLink text={"chat"} />
+        <nav className={navbar}>
+          <CustomNavLink text={"home"} url={"/"} color={"blue"} />
+          <CustomNavLink text={"contact"} url={"/"} color={"blue"} />
+          <CustomNavLink text={"chat"} url={"/"} color={"blue"} />
         </nav>
       </header>
     </div>
   );
 };
 // https://cdn.dribbble.com/users/2146089/screenshots/7030591/media/382396b93b15b30adc250dc220832000.mp4
-export default anding_page;
+export default Landing_page;
