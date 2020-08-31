01eb2c82f1b85f8c1fed81d5c6228ad28d6bd2ad
hobbyluv <princewillchime43@gmail.com>
2020-09-01 00:30:23 +0100

    Updates

---
+++ precache-manifest.b14f457daa9c8cd7f7d8e387d0debb85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c54ff171d7453c5e4b252265e4c572",
+    "url": "/Admin_View_Event_schedule_/index.html"
+  },
+  {
+    "revision": "cc74f9ed6aa68c7db8c8",
+    "url": "/Admin_View_Event_schedule_/static/css/main.6cfd634b.chunk.css"
+  },
+  {
+    "revision": "01b2c672024c1159b03c",
+    "url": "/Admin_View_Event_schedule_/static/js/2.a6b7154a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Admin_View_Event_schedule_/static/js/2.a6b7154a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc74f9ed6aa68c7db8c8",
+    "url": "/Admin_View_Event_schedule_/static/js/main.33cf5f32.chunk.js"
+  },
+  {
+    "revision": "d98046a2538d9115f176",
+    "url": "/Admin_View_Event_schedule_/static/js/runtime-main.314b56b5.js"
+  },
+  {
+    "revision": "034c471e6f09d4adb108dad6ce584970",
+    "url": "/Admin_View_Event_schedule_/static/media/cartoon3.034c471e.jpeg"
+  },
+  {
+    "revision": "07e9dcd373db16f637cf8a3e63a33528",
+    "url": "/Admin_View_Event_schedule_/static/media/manageEvent.07e9dcd3.jpeg"
+  },
+  {
+    "revision": "de02b6348fe881aa87e8296f0b6b1898",
+    "url": "/Admin_View_Event_schedule_/static/media/manageEvent2.de02b634.jpeg"
+  },
+  {
+    "revision": "1c1314a3cea2c0f72d733e92723d3fc3",
+    "url": "/Admin_View_Event_schedule_/static/media/oie_jpg.1c1314a3.png"
+  },
+  {
+    "revision": "1153efa58507f36261156398b9895baf",
+    "url": "/Admin_View_Event_schedule_/static/media/oie_transparent.1153efa5.png"
+  }
+]);
